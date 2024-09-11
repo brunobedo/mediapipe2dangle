3f81e34670e6407d9fd19dbd9fe4b831107e7624
brunobedo <bruno.bedo@usp.br>
2024-09-11 12:02:19 -0300

    update examples

--- example.py
+++ example.py
@@ -2,7 +2,7 @@ import maintools as tools
 import main_run
 
 # Video location
-file = 'videos/video_test_2.mp4'
+file = 'videos/video_test_1.mp4'
 
 # Running Marker-less | Get Hip and Knee 2D Joint Angles
-main_run.run_markerless(video_path=file, side='r', save=True, show=True)
+main_run.run_markerless(video_path=file, side='r', save=True, show=True, scale_factor=0.3)
